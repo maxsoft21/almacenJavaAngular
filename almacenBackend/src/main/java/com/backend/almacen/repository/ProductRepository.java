9cff9da33ec4158d0d578c7e2bc2b0e7cbddabb7
Max Samir Gabriel Leon Quiroz <mleonqui@emeal.nttdata.com>
2022-08-10 19:34:04 -0500

    fix: frontend

--- almacenBackend/src/main/java/com/backend/almacen/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.almacen.repository;
-
-import com.backend.almacen.model.Product;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends PagingAndSortingRepository<Product,Integer> {
-}
